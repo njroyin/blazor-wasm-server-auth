d49bd01d9c12980e419e0bbec8f7e6e0e78a7799
njroyin <njroyin@gmail.com>
2024-10-24 22:12:27 +0800

    Initial commit

---
+++ BlazorWasmServerAuth/BlazorWasmServerAuth.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+builder.Services.AddBootstrapBlazor();
+
+await builder.Build().RunAsync();
---
+++ BlazorWasmServerAuth/BlazorWasmServerAuth/Program.cs
@@ -0,0 +1,41 @@
+﻿using BlazorWasmServerAuth.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddBootstrapBlazor();
+
+//添加权限认证服务
+builder.Services.AddScoped<MyAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<MyAuthenticationStateProvider>());
+builder.Services.AddAuthorizationCore();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorWasmServerAuth.Client._Imports).Assembly);
+
+app.Run();
